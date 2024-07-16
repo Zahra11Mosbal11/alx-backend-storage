ccb3b947183b22fb7ce3e987ffb000b3934f1ea3
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-07-16 13:01:45 -0700

    try

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""  Where can I learn Python?"""
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    return list(mongo_collection.find({"topics": topic}))

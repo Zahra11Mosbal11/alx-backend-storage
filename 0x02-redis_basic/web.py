da3338eaa2035e0eaa13f081a215ccf9a2166375
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-07-18 10:56:27 -0700

    try

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,23 +8,25 @@ from typing import Callable
 
 redis_store = redis.Redis()
 
+
 def data_cacher(method: Callable) -> Callable:
     """Caches the output of fetched data."""
     @wraps(method)
     def invoker(url: str) -> str:
         """The wrapper function for caching the output."""
         redis_store.incr(f'count:{url}')
-        
+
         cached_result = redis_store.get(f'result:{url}')
         if cached_result:
             return cached_result.decode('utf-8')
-        
+
         result = method(url)
-        
+
         redis_store.setex(f'result:{url}', 10, result)
-        
+
         return result
     return invoker
+
 
 @data_cacher
 def get_page(url: str) -> str:
